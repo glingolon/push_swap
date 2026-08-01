e47bdd5726d6c81f3f467700312983e7c7732731
laptop <nicol@Nicola.localdomain>
2026-08-01 13:37:33 +0200

    asdf

---
+++ ignoreinputtest.c
@@ -0,0 +1,21 @@
+#include "push_swap.h"
+
+int main(int argc, char *argv[])
+{
+    t_input input;
+    int i;
+
+    fill_input(&input, argc, argv);
+
+    printf("Bench: %d\n", input.bench);
+    printf("Complexity: %d\n", input.algorithm_complexity);
+    printf("Array Length: %d\n", input.arr_len);
+    
+    printf("Numbers: ");
+    for (i = 0; i < input.arr_len; i++)
+        printf("%d ", input.init_arr[i]);
+    printf("\n");
+
+    // (Free input.init_arr here)
+    return (0);
+}
